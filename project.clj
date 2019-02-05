9586ba334a1df1277b8eb944497dacc21c92757d
joe-rochette <joe.rochette@chubb.com>
2019-02-05 03:09:33 -0600

    dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,13 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  ;; this stuff is really just used for test, i should make
                  ;; another profile.
-                 [org.clojure/data.xml "0.0.8"]
                  [cheshire "5.8.1"]
                  [com.taoensso/timbre "4.10.0"]
-                 ])
+                 ]
+  :profiles {:uberjar {:aot :all}
+             :dev     {:dependencies [[org.postgresql/postgresql "42.2.5"]
+                                      [org.clojure/java.jdbc "0.7.8"]
+                                      [com.taoensso/timbre "4.10.0"]
+                                      [org.clojure/data.xml "0.0.8"]]
+                       :plugins      []}
+             })
