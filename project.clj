1ad830bb858fa6f47087ed90a83dbbb622e5d75a
joe-rochette <joe.rochette@chubb.com>
2019-02-05 03:11:08 -0600

    dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 ;; this stuff is really just used for test, i should make
-                 ;; another profile.
-                 [cheshire "5.8.1"]
-                 [com.taoensso/timbre "4.10.0"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[org.postgresql/postgresql "42.2.5"]
                                       [org.clojure/java.jdbc "0.7.8"]
+                                      [cheshire "5.8.1"]
                                       [com.taoensso/timbre "4.10.0"]
                                       [org.clojure/data.xml "0.0.8"]]
                        :plugins      []}
