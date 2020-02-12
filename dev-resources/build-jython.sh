de295f573e1bcd941a4ce8a6bbd8f29dc4b4fa2c
joefromct <joefromct@users.noreply.github.com>
2020-02-11 23:07:12 -0600

    Update build-jython.sh
    we don't need that.

--- dev-resources/build-jython.sh
+++ dev-resources/build-jython.sh
@@ -2,7 +2,7 @@
 
 
 # Grab jython-standalone, note it's 2.7
-curl --insecure -o ./jython-standalone.jar  https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2b3/jython-standalone-2.7.2b3.jar 
+curl -o ./jython-standalone.jar  https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2b3/jython-standalone-2.7.2b3.jar 
 
 # compile the xmltodict.py file, in Lib
 jython -c "import py_compile; py_compile.compile('Lib/xmltodict.py')"
